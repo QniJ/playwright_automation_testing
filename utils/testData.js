c7b7bac41f749aacc7d52512cb06f06234f026b4
Jenish Maharjan <jenish.maharjan@dogmagroup.co.uk>
2025-08-13 16:00:23 +0545

    Adding utility folder with test data

---
+++ utils/testData.js
@@ -0,0 +1,10 @@
+export const credentials = {
+  valid: {
+    username: "tesuser@gmail.com",
+    password: "password123",
+  },
+  invalid: {
+    username: "test@data.com",
+    password: "passworddata123",
+  },
+};
